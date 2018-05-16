fe3636422a32056a563ab6d8139b301439bfc3b7
arthurnum <arthurnum@gmail.com>
2018-05-16 17:22:41 +0300

    try out prod socket

--- react-message-cli/src/webSocket.js
+++ react-message-cli/src/webSocket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
 
-export const socket = io('ws://localhost:8080');
+// export const socket = io('ws://localhost:8080');
+export const socket = io('ws://react-message-itag.herokuapp.com');
 
 export const emitNewMessage = data => socket.emit('newMessage', JSON.stringify(data))
