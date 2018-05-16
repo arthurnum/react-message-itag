d97d1dde1dc7d483a9043d92b1b85f849c29b6c6
arthurnum <arthurnum@gmail.com>
2018-05-16 17:30:44 +0300

    use default io()

--- react-message-cli/src/webSocket.js
+++ react-message-cli/src/webSocket.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client';
 
 // export const socket = io('ws://localhost:8080');
-export const socket = io('ws://react-message-itag.herokuapp.com');
+// export const socket = io('ws://react-message-itag.herokuapp.com');
+export const socket = io();
 
 export const emitNewMessage = data => socket.emit('newMessage', JSON.stringify(data))
